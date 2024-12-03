0ac0271925b49f854e53c005ea980e5a26b6bc4f
Crisis3012 <crbsk3012@gmail.com>
2024-12-04 00:33:13 +0100

    creador inverted_keywords

--- Adrian Diaz/Filtering.py
+++ Adrian Diaz/Filtering.py
@@ -1,8 +1,8 @@
 import pandas as pd 
 import numpy as np
 
-df1 = pd.read_csv('../data/credits.csv')
-df2 = pd.read_csv('../data/movies_metadata.csv')
+df1 = pd.read_csv('./Data/credits.csv')
+df2 = pd.read_csv('./Data/movies_metadata.csv')
 
 df1.columns = ['id','title','cast','crew']
 df2 = df2.merge(df1, on='id')
