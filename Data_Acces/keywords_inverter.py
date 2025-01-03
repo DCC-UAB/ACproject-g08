8069e215d5370463f720ee5c4a54d0374c641518
Adrimage <adrimage11@gmail.com>
2025-01-03 17:00:51 +0100

    main keywords_inverter

--- Data_Acces/keywords_inverter.py
+++ Data_Acces/keywords_inverter.py
@@ -3,43 +3,44 @@ import numpy as np
 import csv
 import ast
 
-data = {}
+if __name__ == '__main__':
+    data = {}
 
-df = pd.read_csv('./Data/keywords.csv')
+    df = pd.read_csv('./Data/keywords.csv')
 
-lists = list(df["keywords"])
-movies = list(df['id'])
-total = 0
-count = 0
+    lists = list(df["keywords"])
+    movies = list(df['id'])
+    total = 0
+    count = 0
 
-for mid, string in zip(movies, lists):
-    lista = ast.literal_eval(string)
-    total += len(lista)
-    count += 1
-    for dict in lista:
-        try:
-            data[dict['id']]['count'] += 1
-            data[dict['id']]['movies'].append(mid)
-        except:
-            data[dict['id']] = {'id' : dict['id'], 'name' : dict['name'], 'movies': [mid], 'count' : 1}
+    for mid, string in zip(movies, lists):
+        lista = ast.literal_eval(string)
+        total += len(lista)
+        count += 1
+        for dict in lista:
+            try:
+                data[dict['id']]['count'] += 1
+                data[dict['id']]['movies'].append(mid)
+            except:
+                data[dict['id']] = {'id' : dict['id'], 'name' : dict['name'], 'movies': [mid], 'count' : 1}
 
-# Nombre del archivo CSV
-filename = 'inverted_keywords.csv'
+    # Nombre del archivo CSV
+    filename = 'inverted_keywords.csv'
 
-# Abrir el archivo para escritura
-with open(filename, mode='w', newline='', encoding='utf-8') as file:
-    # Obtener los nombres de las columnas
-    fieldnames = list(data[next(iter(data))].keys())
-    
-    # Crear un objeto DictWriter
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
-    
-    # Escribir los encabezados
-    writer.writeheader()
-    
-    # Escribir las filas
-    for key, value in data.items():
-        writer.writerow(value)
+    # Abrir el archivo para escritura
+    with open(filename, mode='w', newline='', encoding='utf-8') as file:
+        # Obtener los nombres de las columnas
+        fieldnames = list(data[next(iter(data))].keys())
+        
+        # Crear un objeto DictWriter
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        
+        # Escribir los encabezados
+        writer.writeheader()
+        
+        # Escribir las filas
+        for key, value in data.items():
+            writer.writerow(value)
 
-print(f'El diccionario se ha guardado en {filename}')
-print(f'El average de keywords por movie es {total/count}')
+    print(f'El diccionario se ha guardado en {filename}')
+    print(f'El average de keywords por movie es {total/count}')
